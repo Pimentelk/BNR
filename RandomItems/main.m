becf5813b35544738ad60029fc00d6ff9c721bc1
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-27 12:52:34 -0500

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Kevin Pimentel on 11/27/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

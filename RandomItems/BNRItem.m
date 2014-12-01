2b1f0ad587b135cb7e355eb48c4969f264fcdef5
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-12-01 16:31:56 -0500

    Big Nerd Ranch sample program

---
+++ RandomItems/BNRItem.m
@@ -0,0 +1,41 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Kevin Pimentel on 11/27/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+- (void)setItemName:(NSString *)str {
+    _itemName = str;
+}
+
+- (NSString *)itemName {
+    return _itemName;
+}
+
+- (void)setSerialNumber:(NSString *)str {
+    _serialNumber = str;
+}
+
+- (NSString *)serialNumber {
+    return _serialNumber;
+}
+
+- (void)setValueInDollars:(int)v {
+    _valueInDollars = v;
+}
+
+- (int)valueInDollars {
+    return _valueInDollars;
+}
+
+- (NSDate *)dateCreated {
+    return _dateCreated;
+}
+
+@end
--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,11 +7,37 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        // Send the message addObject: to the NSMutableArray pointed to
+        // by the variable items, passing a string each time
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObject:atIndex:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        for (NSString *item in items) {
+            // Log the description of item
+            NSLog(@"%@",item);
+        }
+        
+        BNRItem *item = [[BNRItem alloc] init];
+        
+        [item setItemName:@"Red Sofa"];
+        [item setSerialNumber:@"A1B2C"];
+        [item setValueInDollars:100];
+        
+        NSLog(@"%@ %@ %@ %d",[item itemName],[item dateCreated],[item serialNumber],[item valueInDollars]);
+        
+        // Destroy the mutable array object
+        items = nil;
     }
     return 0;
 }

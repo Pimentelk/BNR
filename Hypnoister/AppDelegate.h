4ad9da1bca4b9843527ef57faa56b00f6db547f9
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-12-07 04:30:57 -0500

    Initial Commit

---
+++ Hypnoister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnoister
+//
+//  Created by Kevin Pimentel on 12/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnoister/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hypnoister
+//
+//  Created by Kevin Pimentel on 12/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

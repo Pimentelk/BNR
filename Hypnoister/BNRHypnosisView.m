d092bb48b71a01a253ede20a841ad11bf822738c
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-12-07 05:17:13 -0500

    Renamed files

---
+++ Hypnoister/BNRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  BNRHypnoister.m
+//  Hypnoister
+//
+//  Created by Kevin Pimentel on 12/7/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

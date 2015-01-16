5dd9afc3f0f98f8a4f57d22dd3c4a016bc495028
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2015-01-16 05:18:07 -0500

    Add Hypnosis View Controller

---
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  HypnoNerd
+//
+//  Created by Kevin Pimentel on 1/14/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+#import "BNRHypnosisView.h"
+
+@interface BNRHypnosisViewController ()
+
+@end
+
+@implementation BNRHypnosisViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Set the tab bar item's title
+        self.tabBarItem.title = @"Hypnotize";
+        
+        // Create a UIImage from a file
+        // This will use Hypno@2x.png on retina display devices
+        UIImage *i = [UIImage imageNamed:@"Hypno.png"];
+        
+        // Put that image on the tab bar item
+        self.tabBarItem.image = i;
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loadView {
+    // Create a view
+    BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] init];
+    
+    // Set it as *the* view of this view controller
+    self.view = backgroundView;
+}
+
+@end
